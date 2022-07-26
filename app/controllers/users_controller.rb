f7d5990ac9d991e5ac8cf241f34ce6a267b6bff8
JawaraGordon <jawaragordon@icloud.com>
2022-07-26 10:34:32 -0400

    completed assignment

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+    skip_before_action :authorized_user, only: :create
+
+    def create 
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end 
+
+    def show
+        if current_user 
+            render json: current_user, status: :ok
+        else 
+            render json: {error: "No current user, please login"}, status: :unauthorized
+    end
+end
+
+private
+
+def user_params
+    params.permit(:username, :image_url, :bio, :password, :password_confirmation)
+end 
+
+
+end
